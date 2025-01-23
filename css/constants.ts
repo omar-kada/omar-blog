dfd4c849ef360b86a3fb59c0dc70667c4521b02c
Omar Kada <kadaomar@hotmail.com>
2025-01-23 19:21:47 +0100

    make light border uniform

--- css/constants.ts
+++ css/constants.ts
@@ -3,8 +3,10 @@ const merged = (...strs: string[]) => ` ${twMerge(...strs)} `
 
 export const border = ' rounded border dark:border-gray-700 p-1 '
 export const borderOnMd = ' md:rounded md:border dark:border-gray-700 p-1 '
-export const borderGlow = merged(border, 'dark:border-gray-300 border-gray-700')
-export const borderGlowOnHover = merged(border, 'dark:hover:border-gray-300 hover:border-gray-700')
+export const borderGlow = merged(border, 'dark:border-gray-200 border-gray-700')
+export const borderGlowOnHover = merged(border, 'dark:hover:border-gray-200 hover:border-gray-700')
+
+export const divideStyle = ' divide-gray-200 dark:divide-gray-700 '
 
 export const glow = ' drop-shadow-glow '
 export const glowOnHover = ' hover:drop-shadow-glow '
@@ -26,5 +28,3 @@ export const strongTextOnHover = merged(
   lightText,
   ' group-hover:text-black group-hover:dark:text-gray-100 '
 )
-
-export const divideStyle = ' divide-gray-200 dark:divide-gray-700 '
